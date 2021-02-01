447e6a3f6f9bbd4c717945eec963e8aa26f01577
Louis-Timberlake <l.timberlake@hotmail.com>
2021-02-01 11:52:17 +0000

    Revert "updated to run DataModificationBenchmark"
    This reverts commit 52577451c52042c08b983bca35fb8bdcde765654.

--- map-benchmarks/src/main/java/BenchmarkRunner.java
+++ map-benchmarks/src/main/java/BenchmarkRunner.java
@@ -5,11 +5,10 @@ public class BenchmarkRunner {
 		Benchmark spBenchmark = new StringsPopulationBenchmark();
 		Benchmark rBenchmark = new RetrievalBenchmark();
 		Benchmark erBenchmark = new ExistenceRetrievalBenchmark();
-		Benchmark dmBenchmark = new DataModificationBenchmark();
 		spBenchmark.runBenchmark();
 		rBenchmark.runBenchmark();
 		erBenchmark.runBenchmark();
-		dmBenchmark.runBenchmark();
+
 	}
 
 }
