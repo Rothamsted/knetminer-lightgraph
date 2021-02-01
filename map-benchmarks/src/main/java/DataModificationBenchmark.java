0b0b638af06be54fa9849b73a0602719c953582c
Louis-Timberlake <l.timberlake@hotmail.com>
2021-02-01 11:15:12 +0000

    mostly completed a functional version of DataModification

--- map-benchmarks/src/main/java/DataModificationBenchmark.java
+++ map-benchmarks/src/main/java/DataModificationBenchmark.java
@@ -1,70 +1,81 @@
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.concurrent.ThreadLocalRandom;
 
 import org.apache.commons.lang3.RandomStringUtils;
-import org.mapdb.DB;
-import org.mapdb.DBMaker;
-import org.mapdb.HTreeMap;
-import org.mapdb.Serializer;
 
-public class DataModificationBenchmark {
+public class DataModificationBenchmark extends Benchmark{
+	
+	private int nTest;
+	private int randomNum;
+	private int minLen;
+	private int maxLen;
+	private String generatedValue;
+	private Map<Integer,String> map;
+	
+	void createData() {
+		timeElapsed = 0;
+		for (int i=0; i<10; i++) {
+			minLen = 3;
+			maxLen = 7;
+			int index = data.size();
+			generatedValue = RandomStringUtils.randomAlphanumeric(minLen, maxLen);
+			data.put(index, generatedValue);
+		}			
+	}
 
-	public static void main(String[] args) {
-		DB db = DBMaker
-				.fileDB("file.db")
-				.make();	
-		
-		HTreeMap<String, String> data = db
-				.hashMap("data", Serializer.STRING, Serializer.STRING)
-				.counterEnable()
-				.createOrOpen();
-		
-		long timeElapsed = 0;
-		
-		List<String> keys = new ArrayList<String>();
-		List<String> values = new ArrayList<String>();
-		keys.addAll(data.getKeys());
-		values.addAll(data.getValues());
-		
-		int randomNum = ThreadLocalRandom.current().nextInt(0, 2);
-		int nTest = 0;		
-		int minLen = 3;
-		int maxLen = 7;
-		String generatedValue = "";
-		
-		//TODO: delete and replace need to be finished
+	void printReport() {
+		System.out.println("--- Data Modification ---");
+	    
+		randomNum = ThreadLocalRandom.current().nextInt(0, 2);
+		nTest = 0;		
+		generatedValue = "";
 		
 		while (nTest != 3) {
 			if (randomNum == 0) {
 				//delete
-				
+				int positionDel = ThreadLocalRandom.current().nextInt(0,(data.size()-1));
+				data.remove(positionDel);
+				System.out.println("The operation chosen was: delete" );
+				System.out.println("The position deleted was: " + positionDel);
 				nTest = nTest + 1;
 			} else if (randomNum == 1) {
 				//modify
-				Map.Entry<String,String> entry = data.entrySet();
-			   	String k = entry.getKey();
-			   	String k2= keys.get(0);
-			   	String v = entry.getValue();
-		    	String v2= values.get(0);
-		    	data.replace(k, v2);
+				int positionMod = ThreadLocalRandom.current().nextInt(0,(data.size()-1));
+				generatedValue = RandomStringUtils.randomAlphanumeric(minLen, maxLen);
+		    	data.replace(positionMod, generatedValue);
+		    	System.out.println("The operation chosen was: modify" );
+		    	System.out.println("The key modified was: " + positionMod);
 		    	nTest = nTest + 1;
 			} else {
 				//add
-				String generatedKey = RandomStringUtils.randomAlphanumeric(minLen, maxLen);
 				generatedValue = RandomStringUtils.randomAlphanumeric(minLen, maxLen);
-				data.put(generatedKey, generatedValue);
+				int positionAdd = data.size();
+				data.put(positionAdd, generatedValue);
+				System.out.println("The operation chosen was: add" );
 				nTest = nTest + 1;
 			}	
-		}
-
+			randomNum = ThreadLocalRandom.current().nextInt(0, 2);
+			
+			map = new HashMap<Integer,String>();
+			
+		    for (Entry<Integer, String> entry : data.entrySet()) {
+		    	long start = System.currentTimeMillis();
+		    	Integer k = entry.getKey();
+		    	String v = entry.getValue();
+		    	map.put(k, v);
+		    	long finish = System.currentTimeMillis();
+		    	timeElapsed = timeElapsed + (finish - start);	    	
+		    }
+		    
+		    System.out.println(map);		
+		    
+		}		
 		
-
-
-
+		System.out.println("Took : " + timeElapsed + "ms");	
+		
 	}
 
 }
