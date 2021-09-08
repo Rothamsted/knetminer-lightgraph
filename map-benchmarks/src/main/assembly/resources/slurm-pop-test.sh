97a3f66690af17826224f1380b7199e0a4b0e0b0
Marco Brandizi <marco.brandizi@gmail.com>
2021-09-08 21:15:42 +0100

    Minor fix to test script.

--- map-benchmarks/src/main/assembly/resources/slurm-pop-test.sh
+++ map-benchmarks/src/main/assembly/resources/slurm-pop-test.sh
@@ -8,8 +8,7 @@ do
     echo -e "\n\n\t---- MEMORY: ${mem}G  DATA_SIZE: $data_size\n"
     rm -f file.db
     out=/tmp/slurm-test.out
-    # srun --mem=${mem}G `pwd`/run.sh string-population -s $data_size -c 1000 -l 3 -x 250 \
-		`pwd`/run.sh string-population -s $data_size -c 1000 -l 3 -x 250 \
+    srun --mem=${mem}G `pwd`/run.sh string-population -s $data_size -c 1000 -l 3 -x 250 \
       | tee "$out"
     avg_time=`awk -F ' ' '/Average:/{print $2}' $out`
     echo -e "$mem\t$data_size\t$avg_time" >>$report
